b6ee0e17fed2293997104c8a156a62d5fc2117b4
suvarnasandeep <poojary.udupi@gmail.com>
2023-03-24 09:35:27 +0530

    springboot jpa

--- 2023/udemy_spring_microservices_in28minute/restful-web-services/src/main/java/com/sandeep/webservices/restfulwebservices/user/User.java
+++ 2023/udemy_spring_microservices_in28minute/restful-web-services/src/main/java/com/sandeep/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,23 @@
 package com.sandeep.webservices.restfulwebservices.user;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
+@Entity(name="user_details")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
 	@Size(min=2 ,message="Name should have atleast 2 char")
@@ -14,6 +25,15 @@ public class User {
 	
 	@Past(message="Birth date should be in past")
 	private LocalDate birthDate;
+	
+	@OneToMany(mappedBy = "user")
+	@JsonIgnore
+	private List<UserPost> userPosts;
+	
+	protected User() {
+		
+	}
+	
 	public User(Integer id, String name, LocalDate birthDate) {
 		super();
 		this.id = id;
@@ -38,9 +58,18 @@ public class User {
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
+	
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
+
+	public List<UserPost> getUserPosts() {
+		return userPosts;
+	}
+
+	public void setUserPosts(List<UserPost> userPosts) {
+		this.userPosts = userPosts;
 	}
 	
 	
---
+++ 2023/udemy_spring_microservices_in28minute/restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,23 @@
+insert into user_details(id,birth_date,name)
+values(10001,current_date(),'sandeep');
+
+insert into user_details(id,birth_date,name)
+values(10002,current_date(),'suvarna');
+
+insert into user_details(id,birth_date,name)
+values(10003,current_date(),'niriksha');
+
+insert into user_details(id,birth_date,name)
+values(10004,current_date(),'rakshit');
+
+insert into user_post(id,description,user_id)
+values(20001,'learn aws',10001);
+
+insert into user_post(id,description,user_id)
+values(20002,'learn react',10002);
+
+insert into user_post(id,description,user_id)
+values(20003,'learn boot',10003);
+
+insert into user_post(id,description,user_id)
+values(20004,'learn cloud',10004);
