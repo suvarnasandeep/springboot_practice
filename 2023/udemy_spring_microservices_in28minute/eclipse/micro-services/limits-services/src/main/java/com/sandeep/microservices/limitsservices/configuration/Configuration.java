e01a78d0337d1525d183d3a2a0bbf35e766dcc9c
suvarnasandeep <poojary.udupi@gmail.com>
2023-04-04 23:43:09 +0530

    microservices

---
+++ 2023/udemy_spring_microservices_in28minute/eclipse/micro-services/limits-services/src/main/java/com/sandeep/microservices/limitsservices/configuration/Configuration.java
@@ -0,0 +1,26 @@
+package com.sandeep.microservices.limitsservices.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties("limits-services")
+@Component
+public class Configuration {
+
+	private int minimum;
+	private int maximum;
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	
+	
+}
